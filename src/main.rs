7883723ba345607598e49187e72915e4038aed4c
u296 <u296b3g@gmail.com>
2020-03-08 21:34:09 +0000

    * fixed LICENSE
    * cleanup
    
    * switched vectors to hashsets
    
    * refactoring
    
    * new cmdarg options, to set the install directory and to limit the max amount of download attempts

--- src/main.rs
+++ src/main.rs
@@ -4,54 +4,105 @@ extern crate regex;
 extern crate reqwest;
 extern crate select;
 
+use std::collections::HashSet;
 use std::env;
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::process::exit;
 
-use clap::{App, Arg};
+use clap::{App, Arg, value_t};
 use regex::Regex;
+use reqwest::blocking::{get, Response};
 use select::document::Document;
 use select::predicate::Name;
 
 const APP_INFO: app_dirs::AppInfo = app_dirs::AppInfo { name: "mc_update", author: "u296" };
+const DOWNLOAD_BATCH_SIZE: usize = 5;
 
-const LOCAL_REPO_PATH: &str = "./mc_update";
 
-fn download(url: &str, max_tries: usize) -> Option<reqwest::blocking::Response> {
-    for i in 1..=max_tries {
-        match reqwest::blocking::get(url) {
-            Ok(response) => {
-                println!("info : ({}/{}) get request succeeded for url: {}", i, max_tries, url);
-                return Some(response);
-            }
-            Err(e) => {
-                if let Some(code) = e.status() {
-                    println!("warning ({}/{}): get request failed with code '{}': {}", i, max_tries, code.as_u16(), code.canonical_reason().unwrap());
-                } else {
-                    println!("error: get request failed for other reason");
-                    return None;
-                }
-            }
-        }
-        if i == max_tries {
-            println!("error: {}/{} attempts at downloading file failed", max_tries, max_tries);
-            return None;
-        }
-    };
-    None
-}
-
-fn create_file(path: &Path) -> Option<fs::File> {
+fn create_file(path: &Path) -> Result<fs::File, io::Error> {
     match fs::File::create(&path) {
-        Ok(f) => Some(f),
+        Ok(f) => Ok(f),
         Err(e) => {
             println!("warning: failed to create file '{}': {}", path.display(), e);
-            None
+            Err(e)
         }
     }
 }
+
+
+trait FileWriter {
+    fn write(&mut self, target: &Path) -> Result<(), io::Error>;
+}
+
+impl FileWriter for Response {
+    fn write(&mut self, target: &Path) -> Result<(), io::Error> {
+        match io::copy(self, &mut create_file(target)?) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+impl FileWriter for PathBuf {
+    fn write(&mut self, target: &Path) -> Result<(), io::Error> {
+        match fs::copy(self, target) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+
+fn prompt_continue(msg: &str) {
+    println!("warning: {}\ncontinue? [Y/n]", msg);
+
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("error: failed to read stdin");
+    input.to_ascii_lowercase();
+    if input.contains("n") {
+        exit(0);
+    }
+}
+
+fn download(url: &str, max_tries: &Option<usize>) -> Option<Response> {
+    println!("info: beginning download for url: {}", url);
+    let mut attempt: usize = 1;
+    let upper_bound: String = match max_tries {
+        Some(x) => x.to_string(),
+        None => "∞".to_string()
+    };
+
+    'out: loop {
+        for _ in 0..DOWNLOAD_BATCH_SIZE {
+            match max_tries {
+                Some(x) => {
+                    if attempt > *x {
+                        return None;
+                    }
+                },
+                _ => {}
+            }
+            match get(url) {
+                Ok(response) => {
+                    println!("info: ({}/{}) get request succeeded", attempt, upper_bound);
+                    break 'out Some(response);
+                }
+                Err(e) => {
+                    if let Some(code) = e.status() {
+                        println!("warning: ({}/{}) get request failed with code {}: {}", attempt, upper_bound, code.as_u16(), code.canonical_reason().unwrap_or("no canonical reason"));
+                    } else {
+                        println!("warning: ({}/{}) get request failed", attempt, upper_bound);
+                    }
+                }
+            }
+            attempt += 1;
+        }
+        prompt_continue("batch failed, continue to retry");
+    }
+}
+
 
 fn main() {
     let matches = App::new("mc_update")
@@ -69,13 +120,31 @@ fn main() {
             .help("adds a repository to download to if needed, e.g. './repo/jars'")
             .takes_value(true)
             .multiple(true))
+        .arg(Arg::with_name("max_download_attempts")
+            .short("m")
+            .long("max_download_attempts")
+            .help("sets the max amount of times to try to download a file, defaults to infinite")
+            .takes_value(true))
         .arg(Arg::with_name("JAR_VERSION")
             .help("the jar version to update to, e.g. '1.12.2'")
             .required(true)
             .index(1))
+        .arg(Arg::with_name("INSTALL_DIRECTORY")
+            .help("the directory to install the server.jar into, defaults to '.'")
+            .default_value(".")
+            .index(2))
         .get_matches();
 
     let jar_version = matches.value_of("JAR_VERSION").unwrap().to_string();
+
+    let max_download_attempts = match value_t!(matches, "max_download_attempts", usize)
+        .unwrap_or_else(|e| {
+            prompt_continue(&format!("could not interpret argument 'max_download_attempts' as a number, setting infinite limit: {}", e));
+            std::usize::MAX
+        }) {
+        std::usize::MAX => None,
+        x => Some(x),
+    };
 
     let r = Regex::new(r#"(\.\.)|/|\\"#).unwrap();
 
@@ -84,61 +153,94 @@ fn main() {
         exit(1);
     }
 
+    let local_repo_path = PathBuf::from("./mc_update");
     let global_repo_path = app_dirs::get_app_root(app_dirs::AppDataType::UserCache, &APP_INFO)
         .expect("error: failed to create global cache dir");
+    let install_path = PathBuf::from(matches.value_of("INSTALL_DIRECTORY").unwrap());
 
-    let global_repo_path = global_repo_path.to_str()
-        .unwrap();
+    let mut default_repos = HashSet::new();
+    default_repos.insert(global_repo_path);
+    default_repos.insert(install_path.as_path().join("mc_update"));
+    default_repos.insert(local_repo_path);
 
-    let repos = {
-        let global_repo = Path::new(global_repo_path);
-        let local_repo = Path::new(LOCAL_REPO_PATH);
+    let existing_repos = {
+        let mut existing_repos = HashSet::new();
 
-        let mut repos: Vec<&Path> = Vec::new();
 
-        for repo in [global_repo, local_repo].iter() {
-            if repo.is_dir() {
-                repos.push(repo);
-            }
-        }
+        let user_repos: HashSet<PathBuf> = matches.values_of("repo")
+            .unwrap_or_default()
+            .chain(
+                matches.values_of("install_repo")
+                    .unwrap_or_default()
+            )
+            .map(|x| PathBuf::from(x))
+            .collect();
 
-        for repo in matches.values_of("repo").unwrap_or_default().chain(matches.values_of("install_repo").unwrap_or_default()) {
-            let repo = Path::new(repo);
+
+        for repo in user_repos.union(&default_repos).into_iter() {
             if repo.exists() {
-                if !repo.is_dir() {
-                    repos.push(repo);
+                if repo.is_dir() {
+                    existing_repos.insert(repo.clone());
+                    continue;
                 }
                 println!("warning: '{}' is not a directory, ignoring", repo.display());
             } else {
                 println!("warning: the repo '{}' does not exist", repo.display());
             }
-        }
-        repos
+        };
+        existing_repos
     };
-    let mut jar_file = None;
+    let mut jar_file_path = None;
 
-    'out: for repo in repos.iter() {
-        for file in repo.read_dir().unwrap() {
-            let file = file.unwrap();
-            if file.path().is_file() && file.file_name().into_string().unwrap() == jar_version {
-                jar_file = Some(file.path());
-                break 'out;
+    'out: for repo in existing_repos {
+        for entry in repo.read_dir().expect(format!("error: failed to read directory: '{}'", repo.display()).as_str()) {
+            if let Ok(entry) = entry {
+                if entry.path().is_file() && entry.file_name().as_os_str() == jar_version.as_str() {
+                    jar_file_path = Some(entry.path());
+                    break 'out;
+                }
             }
         }
     }
 
-    if jar_file.is_none() {
-        let mut install_repo: Option<&Path> = None;
+    if let Some(jar_file_path) = jar_file_path {
+        // jar file already on disk
+        println!("info: found cached jar at: {}", jar_file_path.display());
 
-        for repo in matches.values_of("install_repo").unwrap_or_default().chain([global_repo_path, LOCAL_REPO_PATH].iter().copied()) {
-            let repo = Path::new(repo);
+        match fs::copy(jar_file_path, match create_file(&install_path.as_path().join("server.jar")) {
+            Ok(_) => {
+                let h = Path::new(".").join("server.jar");
+                h
+            }
+            Err(_) => {
+                println!("error: failed to create server.jar");
+                exit(1);
+            }
+        }) {
+            Ok(_) => {
+                println!("info: successfully installed version {}", jar_version);
+            }
+            Err(e) => {
+                println!("error: failed to install: {}", e);
+            }
+        }
+    } else {
+        // jar is not on disk
+        let mut install_repo = None;
+
+        let user_repos = matches.values_of("install_repo")
+            .unwrap_or_default()
+            .map(|x| PathBuf::from(x))
+            .collect::<HashSet<PathBuf>>();
+
+        for repo in user_repos.union(&default_repos).into_iter() {
             if repo.exists() {
-                if !repo.is_dir() {
-                    println!("warning: '{}' is not a directory, ignoring", repo.display());
-                    continue;
+                if repo.is_dir() {
+                    install_repo = Some(repo);
+                    break;
                 }
-                install_repo = Some(repo);
-                break;
+                prompt_continue(&format!("potential install repo '{}' is not a directory", repo.display()));
+                continue;
             } else {
                 match fs::create_dir_all(repo) {
                     Ok(_) => {
@@ -147,29 +249,23 @@ fn main() {
                         break;
                     }
                     Err(e) => {
-                        println!("warning: failed to create repo at '{}': '{}'\ncontinue? (Y/n)", repo.display(), e);
-                        let mut input = String::new();
-                        io::stdin().read_line(&mut input).expect("error: failed to read stdin");
-                        input.to_ascii_lowercase();
-                        if input.contains("n") {
-                            exit(0);
-                        }
+                        prompt_continue(&format!("failed to create installation repo at '{}': '{}'", repo.display(), e));
                     }
                 }
             }
         }
 
-        let install_repo = install_repo.unwrap();
+        let install_repo = install_repo.expect("error: failed to select or create an install repo");
 
         let target = format!("https://mcversions.net/download/{}", jar_version);
 
-        let page = match download(target.as_str(), 3) {
+        let page = match download(target.as_str(), &max_download_attempts) {
             Some(x) => x.text().unwrap(),
             _ => exit(1),
         };
 
 
-        let server_jar_url = match Document::from(page.as_str()).find(Name("a"))
+        let jar_url = match Document::from(page.as_str()).find(Name("a"))
             .filter_map(|n| { if n.attr("href")?.contains("server.jar") { Some(n.attr("href")) } else { None } })
             .collect::<Vec<_>>().get(0) {
             Some(link) => link.unwrap(),
@@ -181,90 +277,37 @@ fn main() {
         }
             .to_owned();
 
-        println!("info: server jar at '{}'", server_jar_url);
+        println!("info: server jar at '{}'", jar_url);
 
-        let mut jar_file = match download(server_jar_url.as_str(), 3) {
+        let mut jar_content = match download(jar_url.as_str(), &max_download_attempts) {
             Some(x) => x,
-            _ => exit(1)
+            None => {
+                println!("error: ran out of attempts to download jar");
+                exit(1)
+            }
         };
 
-        match create_file(&install_repo.join(jar_version.as_str())) {
-            Some(mut cache_file) => {
-                match io::copy(&mut jar_file, &mut cache_file) {
-                    Ok(_) => {
-                        match fs::copy(&install_repo.join(jar_version.clone()), match create_file(&Path::new(".").join("server.jar")) {
-                            Some(_) => {
-                                let h = Path::new(".").join("server.jar");
-                                h
-                            }
-                            None => {
-                                println!("error: failed to create server.jar");
-                                exit(1);
-                            }
-                        }) {
-                            Ok(_) => {
-                                println!("info: successfully cached jar in '{}' and installed", install_repo.display());
-                            }
-                            Err(e) => {
-                                println!("error: successfully cached jar in '{}' but failed to install: {}", install_repo.display(), e);
-                            }
-                        }
-                    }
-                    Err(e) => {
-                        match io::copy(&mut jar_file, &mut match create_file(&Path::new(".").join("server.jar")) {
-                            Some(f) => f,
-                            None => {
-                                println!("error: failed to create server.jar");
-                                exit(1);
-                            }
-                        }) {
-                            Ok(_) => {
-                                println!("warning: successfully installed but failed to cache jar: {}", e);
-                            }
-                            Err(e2) => {
-                                println!("error: failed to cache ({}) and failed to install ({})", e, e2);
-                            }
-                        }
-                    }
-                }
-            }
-            None => {
-                println!("warning: failed to cache file in repository '{}'", install_repo.display());
-                match io::copy(&mut jar_file, &mut match create_file(&Path::new(".").join("server.jar")) {
-                    Some(f) => f,
-                    None => {
-                        println!("error: failed to create server.jar");
-                        exit(1);
-                    }
-                }) {
-                    Ok(_) => {
-                        println!("warning: successfully installed but failed to cache jar");
-                    }
-                    Err(e2) => {
-                        println!("error: failed to cache and failed to install: {}", e2);
-                    }
-                }
-            }
-        }
-    } else {
-        let jar_file = jar_file.unwrap();
-        println!("info: found cached jar at: {}", jar_file.display());
+        let mut cache_file_path_buffer = install_repo.join(jar_version.as_str());
+        let mut target_writer: &mut dyn FileWriter = &mut jar_content;
 
-        match fs::copy(&jar_file, match create_file(&Path::new(".").join("server.jar")) {
-            Some(_) => {
-                let h = Path::new(".").join("server.jar");
-                h
-            }
-            None => {
-                println!("error: failed to create server.jar");
-                exit(1);
-            }
-        }) {
+
+        match target_writer.write(&cache_file_path_buffer) {
             Ok(_) => {
-                println!("info: successfully installed version {}", jar_version);
+                target_writer = &mut cache_file_path_buffer;
+                println!("info: successfully cached jar in {}", install_repo.display());
             }
             Err(e) => {
-                println!("error: failed to install: {}", e);
+                println!("warning: failed to cache jar:  {}", e);
+            }
+        }
+
+        match target_writer.write(&install_path.join("server.jar")) {
+            Ok(_) => {
+                println!("info: successfully installed version '{}' into '{}'", jar_version, install_path.display());
+            }
+            Err(e) => {
+                println!("error: failed to install jar into '{}': {}", install_path.display(), e);
+                exit(1);
             }
         }
     }
